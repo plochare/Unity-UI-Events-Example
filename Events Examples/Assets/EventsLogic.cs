8d01f05127233ef5d7a8b14c41d0875a589f1654
Peter Locharernkul <peter@brightlineinteractive.com>
2025-02-13 15:33:15 -0500

    initial commit

---
+++ Events Examples/Assets/EventsLogic.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EventsLogic : MonoBehaviour
+{
+    // Basic Unity Prototyping involves:
+    // Connecting Events --> GameObjects --> Scripting Functions
+
+    // Create Public Variables to expose GameObjects, UI Components, Prefabs and Properties
+    public GameObject playerObj;
+    public Renderer playerRenderer; 
+    public Vector3 currentLocation;
+
+    public Slider scaleSlider;
+    public TMP_Dropdown colorDropdown;
+
+    // Public Array of Custom Materials
+    public Material[] colorPalette; 
+
+    // Frame Events 
+    void Start(){
+        // Set Initial Position
+        playerObj.transform.position = currentLocation;
+
+    }
+    void Update(){
+        // Update every screen refresh
+    }
+    void FixedUpdate(){
+        // Update on the set frame rate
+        currentLocation.y += 0.001f ; // increment y value by 0.001
+        playerObj.transform.position = new Vector3(currentLocation.x, currentLocation.y, currentLocation.z);
+    }
+
+    // Create Public Functions to change or modify Properties
+    public void SetPlayerScale(){
+        playerObj.transform.localScale = new Vector3(scaleSlider.value, scaleSlider.value, scaleSlider.value);
+    }
+
+    public void SetPlayerColor(){
+        playerRenderer.material = colorPalette[colorDropdown.value];
+    }
+
+}
